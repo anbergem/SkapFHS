5995d86a7cf13290cb04a0e0bedcf59890d84fdc
Andreas Bergem <bergem.andreas@gmail.com>
2020-01-28 15:23:52 +0100

    Add solution and progress with class.

--- tictactoe_skeleton.py
+++ tictactoe_skeleton.py
@@ -1,6 +1,9 @@
 class Piece:
     def __init__(self, symbol: str):
-        # TODO: Don't allow symbols of more than one character
+        if type(symbol) != str:
+            raise ValueError(f"Symbol must be a string: {symbol}")
+        elif len(symbol) != 1:
+            raise ValueError(f"Symbol must be 1 letter: {symbol}")
         self.symbol = symbol
 
     def __str__(self) -> str:
@@ -9,7 +12,7 @@ class Piece:
 
     def __eq__(self, other) -> bool:
         """Magic method for checking equality (==)"""
-        pass # TODO
+        return self.symbol == other.symbol
 
     # A static method is one that does not use "self"
     # Therefore, it does not need an instance to be used.
@@ -24,7 +27,8 @@ class Piece:
         return Piece(" ")
 
 class Player:
-    pass # TODO
+    def __init__(self, piece: Piece):
+        self.piece = piece
 
 
 class Board:
@@ -54,18 +58,57 @@ class Board:
 
         return str
 
-    # TODO: More methods
+    def update(self, row: int, column: int, piece: Piece):
+        self._pieces[row][column] = piece
 
+    def get_winner(self):
+        return None
 
+    def is_valid_input(self, row, column):
+        if (0 < row < 3) and (0 < column < 3) and self._pieces[row][column] != Piece.EMPTY(): # Check boundaries
+            return True
+        return False
 
 class Game:
     def __init__(self, board: Board, player_one: Player, player_two: Player):
         self.board = board
-        pass # Todo
+        self.player_one = player_one
+        self.player_two = player_two
+        self.player_ones_turn = True
 
     def play(self):
-        print(self.board)
-        # TODO
+        finished = False
+        while not finished:
+            print(self.board)
+            if self.player_ones_turn:
+                current_player = self.player_one
+            else:
+                current_player = self.player_two
+            row, column = self.ask_for_input(current_player)
+            self.board.update(row, column, current_player.piece)
+            winner = self.board.get_winner()
+            if winner is not None:
+                self.celebrate_victory(winner)
+                finished = True
+
+            self.player_ones_turn = not self.player_ones_turn
+
+    def ask_for_input(self, player: Player):
+        while True:
+            string = input(f"{player}'s turn (row column): ")
+            try:
+                row, column = string.strip().split(' ')
+                row, column = int(row), int(column)
+
+                if self.board.is_valid_input(row, column):
+                    return row, column
+            except:
+                # IF PROGRAM CRASHES
+                print("Invalid input. Please enter a row and column separated by space.")
+
+    def celebrate_victory(self, winner):
+        return
+
 
 """
 This block is used when you want to execute some code in this file. For example if we 
@@ -79,5 +122,7 @@ $ python tictactoe.py
 In all other cases, we don't want to run the following lines. 
 """
 if __name__ == '__main__':
-    game = Game(Board(), Player(), Player())
+    cross = Piece('X')
+    circle = Piece('O')
+    game = Game(Board(), Player(cross), Player(circle))
     game.play()
