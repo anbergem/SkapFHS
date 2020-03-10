b44e3cd635707f1c4c012c81e3293468cb3e2f75
Andreas Bergem <bergem.andreas@gmail.com>
2020-03-10 13:49:31 +0100

    Upgrade reaction to be in different directions.

--- reaction.py
+++ reaction.py
@@ -2,7 +2,17 @@ from sense_hat import SenseHat
 from sensegame import Joystick
 import time
 import random
+import symbols
+from joystick import random_color
 
+choices = {
+    "RIGHT": symbols.right_arrow,
+    "LEFT": symbols.left_arrow,
+    "DOWN": symbols.down_arrow,
+    "UP": symbols.up_arrow,
+    "CENTER": symbols.circle
+}
+keys = list(choices.keys())
 
 class ReactionGame:
     def __init__(self):
@@ -18,16 +28,22 @@ class ReactionGame:
         while True:
             if self.idle:
                 time.sleep(random.randint(1, 100) / 50)
-                self.sense.set_pixel(3, 4, 255, 255, 255)
+                self.key = keys[random.randint(0, 4)]
+                self.color = random_color()
+                self.sense.set_pixels(choices[self.key](self.color))
                 self.counter = time.perf_counter()
                 self.waiting = True
                 self.idle = False
             time.sleep(1)                
             
     def _click(self, button):
-        if (self.waiting):
+        if (self.waiting and self.key == button):
             reaction_time = (time.perf_counter() - self.counter)
-            self.sense.show_message(f'{reaction_time:.2f}')
+            if reaction_time > 59:
+                text = f"{reaction_time//60:.0f}:{reaction_time%60:.2f}"
+            else:
+                text = f'{reaction_time:.2f}'
+            self.sense.show_message(text, text_colour=self.color)
             self.waiting = False
             self.idle = True
             
